e97ed43b6c5d5104c7adfd20bdaa13a3d70ee337
g.punter <g.punter@bboxx.co.uk>
2018-06-27 15:05:52 +0100

    Add some python versions

--- setup.py
+++ setup.py
@@ -96,16 +96,20 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
-    install_requires=REQUIRED,
     url=URL,
     packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
     license='MIT',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
